4f06683603560e9456e6bd6fa0d81947a34c01b9
canvinh2k1 <19020658@vnu.edu.vn>
2022-09-18 23:05:27 +0700

    save run

--- app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
+++ app/src/main/java/com/androiddevs/runningappyt/services/TrackingService.kt
@@ -11,6 +11,7 @@ import android.content.Intent
 import android.location.Location
 import android.os.Build
 import android.os.Looper
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
